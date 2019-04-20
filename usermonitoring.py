7a7fd1441e4331f6f47b354ff5eb77eb0a5678f5
etkinpinar <47389028+etkinpinar@users.noreply.github.com>
2019-04-20 03:56:30 +0200

    Update usermonitoring.py

--- usermonitoring.py
+++ usermonitoring.py
@@ -1,23 +1,29 @@
 ################## METADATA ##################
-# NAME: Etkin Pınar
+# NAME: Etkin Pinar
 # USERNAME: a18etkpi
 # COURSE: Scriptprogramming IT384G - Spring 2019
 # ASSIGNMENT: Assignment 1 - Python
-# DATE OF LAST CHANGE: 2019-04-19
+# DATE OF LAST CHANGE: 2019-04-20
 ##############################################
+
 import pwd
+from systemd import login
 
 def listusers():
     userList = []
     data = pwd.getpwall()
     for info in data:
         user = {
-            "UID": info[2],
-            "username": info[0],
-            "shell" :info[6]
-            #,"status": True
+            'UID': info[2], 
+            'username': info[0], 
+            'shell': info[6]
         }
+        for online in login.uids():
+            if online == info[2]:
+               user['status']= True
+               break
+            else:
+                user['status']= False
         userList.append(user)
+    userList = sorted(userList, key=lambda k: k['UID'])
     return userList
-
-for user in listusers():print(user)
