da8721228bf932b1beb2917323b3089587d06f79
etkinpinar <47389028+etkinpinar@users.noreply.github.com>
2019-05-23 00:26:17 +0200

    Update Script_E.py

--- Script_E.py
+++ Script_E.py
@@ -3,7 +3,7 @@
 # USERNAME: a18etkpi
 # COURSE: Scriptprogramming IT384G - Spring 2019
 # ASSIGNMENT: Assignment 1 - Python
-# DATE OF LAST CHANGE: 2019-04-09
+# DATE OF LAST CHANGE: 2019-05-23
 ##############################################
 
 alphabet = {}
