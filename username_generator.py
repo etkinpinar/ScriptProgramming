b5babcc05c542b94de236bdcbb8b4a716bc4841a
etkinpinar <47389028+etkinpinar@users.noreply.github.com>
2019-05-28 22:14:16 +0200

    Add files via upload

--- username_generator.py
+++ username_generator.py
@@ -1,3 +1,11 @@
+################## METADATA ##################
+# NAME: Etkin Pınar
+# USERNAME: a18etkpi
+# COURSE: Scriptprogramming IT384G - Spring 2019
+# ASSIGNMENT: Assignment 1 - Python
+# DATE OF LAST CHANGE: 2019-05-28
+##############################################
+
 import datetime
 from unidecode import unidecode
 
@@ -8,21 +16,22 @@ def nameSplitter(name):
     surname = partedname[2].strip().lower()
     return (name, surname)
 
-#creates a list that contains letters in the alphabet   
+#creates a list that contains letters in the alphabet
 alphabet = []
 for i in range(97,123):
     alphabet.append(chr(i))
 
+namefile = "name_list.txt"
 names = []
 #gets users' names from text file
-with open ("users.txt", "r") as usersfile:
+with open (namefile, "r") as usersfile:
     for line in usersfile:
         if not line.isspace(): names.append(unidecode(line.strip()))
-             
+
 year = datetime.datetime.now().strftime("%y")
 
 #this dictionary maps usernames with their number
-usernamedict = {}   
+usernamedict = {}
 for name in names:
     nametuple = nameSplitter(name)
     #gets name from the tuple
@@ -35,10 +44,12 @@ for name in names:
     #if surname is only one letter, it adds that letter so that we can have at least 2 letters
     if not len(surname) == 2:
         surname = surname + surname
-    #creates username 
+    #creates username
     username = name[:3] + surname[:2]
     #when it creates a username, it increases the number for that username in the dictionary
     usernamedict[username] = usernamedict.setdefault(username, 0) + 1
     #creates final username
     unique_username = alphabet[usernamedict[username]-1] + year + username
-    print(unique_username)
+    #writes it to a text file
+    with open("unique_usernames.txt", "a") as f:
+        f.write(unique_username+'\n')
